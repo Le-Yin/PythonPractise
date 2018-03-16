3d1cd0d430b3f317c2ffd2ce489ab8fe77fae1a9
Le Yin <chelseain@icloud.com>
2018-03-16 16:08:23 +0800

    init import

---
+++ venv/bin/com/le/yin/test/Test.py
@@ -0,0 +1,71 @@
+print("This line will be printed!")
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1")
+
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+
+mystring = "Don't worry about apostrophes"
+print(mystring)
+
+hello = "hello"
+world = "world"
+one = 1
+helloworld = hello + world + str(one)
+print(helloworld)
+
+
+a, b = 3, 4
+print(a, b)
+
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+print("%f and %d" % (myfloat, myint))
+
+mylist = [2, 3]
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+for x in mylist:
+    print(x)
+
+print(mylist.count(2))
+
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+remainder = 11 % 3 * 2
+print(remainder)
+
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+lotsofhello = "hello" * 10
+print(lotsofhello)
+
+
+even_number = [2,4,6,8]
+odd_number = [1,3,5,7]
+all_numbers = even_number + odd_number
+all_numbers.sort()  # modify original lists in place
+print(all_numbers)
+all_number_new = sorted(all_numbers) # return a new lists
+print(all_number_new)
---
+++ venv/bin/com/le/yin/test/Test1.py
@@ -0,0 +1,33 @@
+statement = "This is a test string from Andrew"
+sortedStatement = sorted(statement.split(), key=str.lower)
+print(sortedStatement)
+
+print([1,2,3] * 3)
+
+x = object()
+y = object()
+
+x_list = [x]
+x_list = x_list * 10
+
+y_list = [y]
+y_list = y_list * 10
+
+big_list = x_list + y_list
+
+print("x_list cotains %d objects" % len(x_list))
+print("y_list cotains %d objects" % len(y_list))
+print("big_list cotains %d objects" % len(big_list))
+
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
+
+mylist = [1,2,3]
+print("A list : %s" % mylist)
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%.2f"
+
+print(format_string % data)
