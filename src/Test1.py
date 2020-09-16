38586549373900afd917c61ce198a8dec358941a
swedge <scott.wedge@gmail.com>
2020-09-15 23:05:37 -0400

    Fixes #1

--- src/Test1.py
+++ src/Test1.py
@@ -15,9 +15,9 @@ y_list = y_list * 10
 
 big_list = x_list + y_list
 
-print("x_list cotains %d objects" % len(x_list))
-print("y_list cotains %d objects" % len(y_list))
-print("big_list cotains %d objects" % len(big_list))
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
 
 if x_list.count(x) == 10 and y_list.count(y) == 10:
     print("Almost there...")
